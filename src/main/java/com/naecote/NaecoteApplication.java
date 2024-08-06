ddedfde4fb7a32f79d3a8f02013b7f6f53a28186
oneseel <seno10065@gmail.com>
2024-08-06 14:13:42 +0900

    first commit

---
+++ src/main/java/com/naecote/NaecoteApplication.java
@@ -0,0 +1,13 @@
+package com.naecote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaecoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaecoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naecote/NaecoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naecote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaecoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
